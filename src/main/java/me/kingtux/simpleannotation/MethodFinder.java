1e6f40ea5a0115f314a0c451f21e463dec90cc00
wherkamp <wherkamp@kingtux.me>
2018-11-09 12:38:50 -0800

    Starting on making the code cleaner

---
+++ src/main/java/me/kingtux/simpleannotation/MethodFinder.java
@@ -0,0 +1,9 @@
+package me.kingtux.simpleannotation;
+
+public class MethodFinder {
+//getAllMethodWithAnnotation(Class class, Annotation annotation)
+//getAllMethodWithAnnotation(Class class, Annotation annotation, checkPrivate)
+//getFirstMethodWithAnnotation(Class class, Annotation annotation{
+//getFirstMethodWithAnnotation(Class class, Annotation annotation, boolean checkPrivate}
+
+}
